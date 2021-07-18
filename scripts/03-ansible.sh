a87a2a0d3deffb8d21a7b4935566dac2e72a744a
MATSUU Takuto <matsuu@gmail.com>
2021-07-18 09:08:45 +0900

    use my fixed repository

--- scripts/03-ansible.sh
+++ scripts/03-ansible.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y ansible curl git 
 
-git clone https://github.com/isucon/isucon10-qualify.git
+git clone --depth=1 -b fixed https://github.com/matsuu/isucon10-qualify.git
 (
 	cd isucon10-qualify/provisioning/ansible
 	ansible-playbook --connection=local -i allinone, allinone.yaml
